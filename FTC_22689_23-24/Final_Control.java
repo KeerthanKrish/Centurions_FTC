04c2146fe2b47ba8aa9a089d3f8a5a42b055c6f0
KeerthanKrish <58368197+KeerthanKrish@users.noreply.github.com>
2024-03-27 15:42:42 -0500

    Update Final_Control.java

--- FTC_22689_23-24/Final_Control.java
+++ FTC_22689_23-24/Final_Control.java
@@ -23,7 +23,7 @@ public class Final_Control extends LinearOpMode {
     private DcMotor left_drive_back;
     private DcMotor right_drive_back;
     private DcMotor left_drive_front;
-    private DcMotor right_drive_front; // TODO: Need to just copy code from other file into this - get the expansion hub cable set up to test properly
+    private DcMotor right_drive_front;
     private DcMotor left_shoulder;
     private DcMotor right_shoulder;
     private Servo wrist;
@@ -38,13 +38,26 @@ public class Final_Control extends LinearOpMode {
         left_drive_back = hardwareMap.get(DcMotor.class, "motor_back_left");
         right_drive_back = hardwareMap.get(DcMotor.class, "motor_back_right");
         
-        // Shoulders
+        // Shoulders (w/ encoders)
         left_shoulder = hardwareMap.get(DcMotor.class, "left_shoulder");
+        left_shoulder.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         right_shoulder = hardwareMap.get(DcMotor.class, "right_shoulder");
+        right_shoulder.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         
-        //Wrist (Servo Motor)
+        //Wrist & Hand(Servo Motor)
         wrist = hardwareMap.get(Servo.class, "wrist_servo");
         hands = hardwareMap.get(Servo.class, "hands_servo");
+        
+        //Setting initial positions
+        wrist.setPosition(1);
+        hands.setPosition(0.5);
+        left_shoulder.setTargetPosition(0);
+        right_shoulder.setTargetPosition(0);
+        left_shoulder.setPower(-0.2);
+        right_shoulder.setPower(0.2);
+        left_shoulder.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        right_shoulder.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        
         
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
@@ -54,7 +67,10 @@ public class Final_Control extends LinearOpMode {
         double powerRight = 0;
         
         while (opModeIsActive()) {
+            //Displaying encoder ticks for shoulder
             telemetry.update();
+            telemetry.addData("RIGHT Motor Ticks: ", right_shoulder.getCurrentPosition());
+            telemetry.addData("LEFT Motor Ticks: ", left_shoulder.getCurrentPosition());
             
             // Getting input from gamePad
             powerLeft = this.gamepad2.left_stick_y;
@@ -68,6 +84,7 @@ public class Final_Control extends LinearOpMode {
             right_drive_front.setPower(-powerRight);
             right_drive_back.setPower(-powerRight);
             
+            //Crabbing right
             if(gamepad2.dpad_right){
                 left_drive_front.setPower(-0.9);
                 left_drive_back.setPower(1);
@@ -75,6 +92,7 @@ public class Final_Control extends LinearOpMode {
                 right_drive_back.setPower(1);
             }
             
+            //Crabbing left
             if(gamepad2.dpad_left){
                 left_drive_front.setPower(0.9);
                 left_drive_back.setPower(-1);
@@ -82,21 +100,85 @@ public class Final_Control extends LinearOpMode {
                 right_drive_back.setPower(-1);
             }
             
+            //Close hand
             if (this.gamepad1.x)
             {
-                hands.setPosition(0.98);
+                hands.setPosition(0.8);
             }
+            
+            //Open hand
             if (this.gamepad1.b)
             {
-                hands.setPosition(0.7);   
+                hands.setPosition(0.5);   
             }
-            if (this.gamepad1.y)
+            
+            //Put wrist down    
+            if (this.gamepad1.a)
             {
                 wrist.setPosition(1);
             }
-            if (this.gamepad1.a)
+            
+            //Pull wrist up
+            if (this.gamepad1.y)
             {
                 wrist.setPosition(0);   
+            }
+            
+            //Move arm up
+            if(gamepad1.dpad_up && left_shoulder.getCurrentPosition() < 1400 && right_shoulder.getCurrentPosition() > -1400){
+                int curPosL = left_shoulder.getCurrentPosition();
+                int curPosR = right_shoulder.getCurrentPosition();
+                left_shoulder.setTargetPosition((int)curPosL+20);
+                right_shoulder.setTargetPosition((int)curPosR-20);
+                left_shoulder.setPower(1);
+                right_shoulder.setPower(-1);
+                left_shoulder.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                right_shoulder.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            }
+            
+            //Move arm down
+            if(gamepad1.dpad_down && left_shoulder.getCurrentPosition() > -2 && right_shoulder.getCurrentPosition() < 2){
+                int curPosL = left_shoulder.getCurrentPosition();
+                int curPosR = right_shoulder.getCurrentPosition();
+                left_shoulder.setTargetPosition((int)curPosL-20);
+                right_shoulder.setTargetPosition((int)curPosR+20);
+                left_shoulder.setPower(-1);
+                right_shoulder.setPower(1);
+                left_shoulder.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                right_shoulder.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            }
+            
+            //Move arm and wrist position to board position
+            if(gamepad1.dpad_right && left_shoulder.getCurrentPosition() < 1400 && right_shoulder.getCurrentPosition() > -1400){
+                left_shoulder.setTargetPosition(540);
+                right_shoulder.setTargetPosition(-540);
+                left_shoulder.setPower(0.3);
+                right_shoulder.setPower(0.3);
+                left_shoulder.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                right_shoulder.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                wrist.setPosition(0.8);
+            }
+            
+            //Send arm to home (down) position
+            if(gamepad1.left_stick_y == 1){
+                left_shoulder.setTargetPosition(0);
+                right_shoulder.setTargetPosition(0);
+                left_shoulder.setPower(-0.2);
+                right_shoulder.setPower(0.2);
+                left_shoulder.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                right_shoulder.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                
+            }
+            
+            //Send arm to max (up) position
+            if(gamepad1.left_stick_y == -1){
+                left_shoulder.setTargetPosition(1399);
+                right_shoulder.setTargetPosition(-1399);
+                left_shoulder.setPower(0.2);
+                right_shoulder.setPower(-0.2);
+                left_shoulder.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                right_shoulder.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                
             }
         }
     }
